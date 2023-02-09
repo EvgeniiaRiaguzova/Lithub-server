83b5da7eb7c6b96d588638a5f72dd121b5afa0b4
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-02-09 20:41:26 +0100

    Add files via upload

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const isLoggedIn = require('../middleware/isLoggedIn')
-const fileUploader = require("../config/cloudinary.config");
+
 
 const Booke = require('../models/Book.model');
 const Comment = require('../models/Comment.model'); 
