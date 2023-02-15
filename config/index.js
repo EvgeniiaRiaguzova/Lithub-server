c6f8beb11458536236a580b1feac3cded5f910a6
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2023-02-15 12:33:17 +0100

    MONGO DB URI, process.env.ORIGIN

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: ["http://localhost:3000", process.env.ORIGIN],
     })
   );
 
