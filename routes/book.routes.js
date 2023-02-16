64c4688852fbcc66b23141d88526e78cc0cf8912
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-02-16 18:26:16 +0100

    Add files via upload

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -2,37 +2,36 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const isLoggedIn = require('../middleware/isLoggedIn')
-const fileUploader = require("../config/cloudinary.config");
 
-const Booke = require('../models/Book.model');
+
+const Book = require('../models/Book.model');
 const Comment = require('../models/Comment.model'); 
 const User = require('../models/User.model');
 
 
-router.get('/books', (req, res) => {
+router.get('/', (req, res) => {
   Book.find()
     .then(books => res.json(books))
     .catch(err => res.status(404).json({ nobooksfound: 'No Books found' }));
 });
 
 
-router.get('books/:booksId', (req, res) => {
-  Book.findById(req.params.id)
+router.get('/:booksId', (req, res) => {
+  Book.findById(req.params.booksId)
     .then(book => res.json(book))
     .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
 });
 
 
-router.post('/books', (req, res) => {
+router.post('/', (req, res) => {
   Book.create(req.body)
     .then(book => res.json({  message: 'Book added successfully' }))
     .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
 });
 
 //update book
-router.put('books/:booksId', (req, res) => {
-  Book.findByIdAndUpdate(req.params.id, req.body)
+router.put('/:booksId', (req, res) => {
+  Book.findByIdAndUpdate(req.params.booksId, req.body)
     .then(book => res.json({  message: 'Updated successfully' }))
     .catch(err =>
       res.status(400).json({ error: 'Unable to update the Database' })
@@ -40,8 +39,8 @@ router.put('books/:booksId', (req, res) => {
 });
 
 //Delete book
-router.delete('books/:booksId', (req, res) => {
-  Book.findByIdAndRemove(req.params.id, req.body)
+router.delete('/:booksId', (req, res) => {
+  Book.findByIdAndRemove(req.params.booksId, req.body)
     .then(book => res.json({  message: 'Book deleted successfully' }))
     .catch(err => res.status(404).json({ error: 'No book found' }));
 });
