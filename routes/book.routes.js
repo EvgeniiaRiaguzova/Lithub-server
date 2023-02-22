0e657662c71ef9a5808f1f2419e80689b2bf0d0b
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-02-22 19:10:41 +0100

    Add files via upload

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
 
 
 
@@ -17,19 +18,37 @@ router.get('/', (req, res) => {
 
 
 router.get('/:booksId', (req, res) => {
-
   Book.findById(req.params.booksId).populate("comments").populate("author")
+ 
 
     .then(book => res.json(book))
     .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
 });
 
-//        /api/books
-router.post('/', (req, res) => {
-  console.log("heeey")
-  Book.create(req.body)
-    .then(book => res.json({  message: 'Book added successfully' }))
+
+router.post('/', isAuthenticated , async (req, res) => {
+console.log("the payload" ,req.payload)
+  const bookCreated = {
+    title: req.body.title,
+    genre:req.body.genre,
+    description:req.body.description,
+    author: req.payload._id
+  }
+
+  const book = await Book.create(bookCreated)
+
+  const user = await User.findById(req.payload._id).populate("books")
+  user.books.push(book._id)
+  await user.save()
+
+  res.json({message: 'Book added successfully', user})
+  
+/*
+  Book.create(bookCreated)
+    .then(book => user.books.push(book._id))
+    .then( res.json({  message: 'Book added successfully' }))
     .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
+    */
 });
 
 //update book
